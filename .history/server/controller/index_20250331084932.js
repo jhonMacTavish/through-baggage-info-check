04c2ba9d92ae3cac4c4641170baac38022c33d62
john_mactavish <981192661@qq.com>
2025-03-31 15:38:58 +0800

    优化查询sql

--- .history/server/controller/index_20250331084932.js
+++ .history/server/controller/index_20250331084932.js
@@ -1,11 +1,3 @@
-/*
- * @Author: john_mactavish 981192661@qq.com
- * @Date: 2025-03-27 10:00:00
- * @LastEditors: john_mactavish 981192661@qq.com
- * @LastEditTime: 2025-03-31 08:49:32
- * @FilePath: \through-baggage-webe:\projects_vscode\company\through-baggage-info-check\server\controller\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 var connector = require('../utils/connector');
 
 const queryThroughBaggageInfo = async () => {
